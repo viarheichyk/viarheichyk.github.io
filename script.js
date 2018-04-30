40f041e5dc9540722b6fb338e8a9a2eae7bc5749
viarheichyk <aliaksandr.viarheichyk@gmail.com>
2018-05-01 01:59:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+document.ready(function(){
   VK.init(function() { 
-     document.getElementBuId("info").innerHtml = 'done';
-  }, function() { 
-     alert("error!");
-}, '5.74'); 
+       document.getElementBuId("info").innerHtml = 'done';
+    }, function() { 
+       alert("error!");
+  }, '5.74'); 
+});
