a8985cab74dda3f6154a79b8a52ce51f3bd7824c
viarheichyk <aliaksandr.viarheichyk@gmail.com>
2018-05-01 01:56:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-VK.init(function(){
-  VK.api("account.getProfileInfo", null, function(data){
-    document.getElementById("info").innerHtml = data.response.first_name;
-  });
-});
+  VK.init(function() { 
+     document.getElementBuId("info").innerHtml = 'done';
+  }, function() { 
+     // API initialization failed 
+     // Can reload page here 
+}, '5.74'); 
